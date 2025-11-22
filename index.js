1408b9682ecbed9e1b99d3510041980a40acb4b1
slonepearson <Slonepearson1@gmail.com>
2025-11-22 13:11:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
+const homeScore = document.getElementById("home-score")
+const guestScore = document.getElementById("guest-score") 
+const incrementBtn = document.querySelector("#increment-btn")
+const newGameBtn = document.querySelector("#new-game")
+
 let homeCount = 0
 let guestCount = 0
-let homeScore = document.getElementById("home-score")
-let guestScore = document.getElementById("guest-score") 
 
-function addPoints(team, points) {
-    if (team === 'home'){
-        homeCount += points
-        homeScore.textContent = homeCount
-    } else if (team === 'guest') {
-        guestCount += points
-        guestScore.textContent = guestCount
-    }
-    updateWinningTeam()
-}
-
-function newGame() {
-    homeCount = 0
-    guestCount = 0
-    homeScore.textContent = homeCount
-    guestScore.textContent = guestCount
-    updateWinningTeam() // Reset colors too
-}
-
-function updateWinningTeam() {
+const updateWinningTeam = () => {
     if (homeCount > guestCount) {
         homeScore.style.color = '#39FF14'
         guestScore.style.color = ""
@@ -34,3 +18,32 @@ function updateWinningTeam() {
         guestScore.style.color = ""
     }
 }
+
+const addPoints = () => {
+
+    const team = incrementBtn.name
+    const points = parseInt(incrementBtn.value, 10)
+
+    switch (team){
+        case "home":
+            homeCount += points
+            homeScore.textContent = homeCount
+            break
+        case "away":
+            guestCount += points
+            guestScore.textContent = guestCount
+            break
+    }
+    updateWinningTeam()
+}
+
+const newGame = () => {
+    homeCount = 0
+    guestCount = 0
+    homeScore.textContent = homeCount
+    guestScore.textContent = guestCount
+    updateWinningTeam()
+}
+
+incrementBtn.addEventListener('click', addPoints)
+newGameBtn.addEventListener('click', newGame)
